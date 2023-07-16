a1f1fe2872e5f645d02610e503f0e1d5f32515f7
TomasNagy <tomi.nagy@outlook.com>
2023-07-16 13:23:34 +0200

    feat: implement reading CL arguments

--- recover.c
+++ recover.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_CHARS 50
 
 int main(int argc, char *argv[])
 {
-  
+  if (argc != 3)
+  {
+    printf ("Usage: <program> <input.raw> <output_folder_path>\n");
+    return EXIT_FAILURE;
+  }
+
+  char input[MAX_CHARS];
+  char output[MAX_CHARS];
+
+  strcpy_s(input, sizeof(input), argv[1]);
+  strcpy_s(output, sizeof(output), argv[2]);
+
+
+
   return EXIT_SUCCESS;
 }
